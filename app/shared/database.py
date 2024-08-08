f892c3f130f9ef872531e583620a35dc329364a1
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-08-08 18:15:19 -0400

    feat: atualizando url do db

--- app/shared/database.py
+++ app/shared/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "postgresql+psycopg2://admin:password@localhost/fighting_games_db"
+DB_URL = "postgresql://admin:kPPeCR42zp6c38WIDXDY92L35BLbso9F@dpg-cqqjvirv2p9s73b8q9jg-a.oregon-postgres.render.com/fg_games_db"
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
