4815ce3b1d8b69aa97e78a8a31505a43324efed9
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-08-09 10:08:01 -0400

    feat: Arrumando engine

--- app/shared/database.py
+++ app/shared/database.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 DB_URL = "postgresql://admin:kPPeCR42zp6c38WIDXDY92L35BLbso9F@dpg-cqqjvirv2p9s73b8q9jg-a.oregon-postgres.render.com/fg_games_db"
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
